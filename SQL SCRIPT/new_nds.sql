0f46b3e666f241cf9b5b2731b540eac10586edcd
dhuy00 <huyduc480@gmail.com>
2024-12-20 17:42:43 +0700

    update sql script

---
+++ SQL SCRIPT/new_nds.sql
@@ -0,0 +1,99 @@
+﻿--Tạo Stage
+use master 
+go
+if DB_ID('NDS') IS NOT NULL
+	ALTER DATABASE NDS SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE NDS;
+GO
+CREATE DATABASE NDS
+GO
+USE NDS
+GO
+
+SELECT * FROM SYS.TABLES
+
+DROP TABLE IF EXISTS AirQualityData_NDS
+DROP TABLE IF EXISTS Counties_NDS
+DROP TABLE IF EXISTS State_NDS
+
+CREATE TABLE State_NDS (
+	State_SK INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+    State_code INT,
+    StateName NVARCHAR(100),
+    State_id NVARCHAR(5),
+	Created DATETIME,
+    LastUpdated DATETIME,
+	SourceID INT,
+	Status INT,
+);
+GO
+SELECT * FROM State_NDS
+
+
+CREATE TABLE Counties_NDS (
+	County_SK INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+    County_fips CHAR(5),
+    County NVARCHAR(255),
+    County_ascii NVARCHAR(255),
+    County_full NVARCHAR(255),
+    State INT,
+    Lat DECIMAL(10, 6),
+    Lng DECIMAL(10, 6),
+    population INT,
+	Created DATETIME,
+    LastUpdated DATETIME,
+	SourceID INT,
+	Status INT,
+
+	--KHOA NGOAI 
+	CONSTRAINT FK_Counties_NDS_State FOREIGN KEY(State) REFERENCES State_NDS(State_SK)
+);
+GO
+
+CREATE TABLE AirQualityData_NDS (
+	AQI_SK INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+	AQI_ID INT,
+    County INT,
+    AQI INT,
+    Category NVARCHAR(50),
+	Date DATETIME,
+    DefiningParameter NVARCHAR(100),
+    DefiningSite NVARCHAR(100),
+    NumberOfSitesReporting INT,
+    Created DATETIME,
+    LastUpdated DATETIME,
+	SourceID INT,
+	Status INT
+	--KHOA NGOAI
+	CONSTRAINT FK_AirQualityData_NDS_Counties FOREIGN KEY (County) REFERENCES Counties_NDS(County_SK),
+);
+
+CREATE TABLE AQI_TEMP (
+    [Row_ID] int,
+    [AQI] int,
+    [Category] nvarchar(50),
+    [DefiningParameter] nvarchar(100),
+    [DefiningSite] nvarchar(100),
+    [NumberOfSitesReporting] int,
+    [County_fips] CHAR(5),
+    [Status] int,
+    [Date] date
+)
+
+-------------------------------------------------------------------------
+SELECT * FROM SYS.TABLES
+
+select * from AirQualityData_NDS
+SELECT * FROM State_NDS
+SELECT * FROM Counties_NDS
+
+
+
+
+
+
+
+
+
+
+
---
+++ SQL SCRIPT/new_stage.sql
@@ -0,0 +1,56 @@
+﻿--Tạo Stage
+use master 
+go
+if DB_ID('STAGE') IS NOT NULL
+	ALTER DATABASE STAGE SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE  STAGE
+GO
+CREATE DATABASE STAGE
+GO
+USE STAGE
+GO
+
+SELECT * FROM SYS.TABLES
+
+DROP TABLE IF EXISTS AirQualityData_Stage
+DROP TABLE IF EXISTS Counties_Stage
+
+CREATE TABLE AirQualityData_Stage (
+	Row_ID INT IDENTITY(1, 1) NOT NULL,
+    StateName NVARCHAR(100),
+    CountyName NVARCHAR(100),
+    StateCode NVARCHAR(10),
+    CountyCode NVARCHAR(10),
+    Date DATE,
+    AQI INT,
+    Category NVARCHAR(50),
+    DefiningParameter NVARCHAR(100),
+    DefiningSite NVARCHAR(100),
+    NumberOfSitesReporting INT,
+    Created DATETIME,
+    LastUpdated DATETIME,
+	SourceID INT,
+	Status INT,
+	County_fips CHAR(5),
+);
+
+
+SELECT * FROM AirQualityData_Stage
+
+CREATE TABLE Counties_Stage (
+    County_fips CHAR(5) PRIMARY KEY,
+    County NVARCHAR(255),
+    County_ascii NVARCHAR(255),
+    County_full NVARCHAR(255),
+    State_id NVARCHAR(5),
+    State_name NVARCHAR(255),
+    Lat DECIMAL(10, 6),
+    Lng DECIMAL(10, 6),
+    population INT,
+	Created DATETIME,
+    LastUpdated DATETIME,
+	SourceID INT,
+	Status INT,
+);
+
+select * from Counties_Stage
